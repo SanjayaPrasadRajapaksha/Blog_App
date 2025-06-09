4af02cfe718cb36c9e74b027a1ac9c2a9c6c7702
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2025-06-09 22:23:34 +0530

    Implement blog delete API

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -2,6 +2,7 @@ import { ConnectDB } from "@/lib/config/db";
 import { NextResponse } from "next/server";
 import { writeFile } from "fs/promises";
 import BlogModel from "@/lib/models/BlogModel";
+const fs = require('fs')
 
 
 const LoadDB = async () => {
@@ -42,4 +43,12 @@ export async function POST(request) {
     await BlogModel.create(blogdata);
 
     return NextResponse.json({ success: true, msg: "Blog Added" })
+}
+
+export async function DELETE(request) {
+    const id = await request.nextUrl.searchParams.get('id');
+    const blog = await BlogModel.findById(id);
+    fs.unlink(`/public${blog.image}`);
+    await BlogModel.findByIdAndDelete(id);
+    return NextResponse.json({ msg: "Blog Deleted" })
 }
