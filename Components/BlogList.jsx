26c80060d458136665a1ea55cf4106f2432a2fd0
Sanjaya Prasad <130231733+SanjayaPrasadRajapaksha@users.noreply.github.com>
2025-06-10 14:01:42 +0530

    Implement and finalize all planned features

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -27,7 +27,7 @@ const BlogList = () => {
             </div>
             <div className='flex flex-wrap justify-around gap-y-10 mb-16 xl:mx:24'>
                 {blog.filter((item) => menu === 'All' ? true : item.category === menu).map((item, index) => {
-                    return <BlogItem key={index} id={item._id} image={item.image} title={item.title} descriptin={item.description} category={item.category} />
+                    return <BlogItem key={index} id={item._id} image={item.image} title={item.title} description={item.description} category={item.category} />
                 })
 
                 }
